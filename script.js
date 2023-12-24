b773d9d684d369eeaa214c8c3c7814a12655ed1b
PhuongHM <40162878+hmp18091996@users.noreply.github.com>
2023-12-24 21:29:33 +0700

    Update index

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-const myCarouselElement = document.querySelector('#myCarousel')
 
-const carousel = new bootstrap.Carousel(myCarouselElement, {
-    interval: 2000,
-    touch: false
-})
+// Open Popup Listener
+document.addEventListener("DOMContentLoaded", function () {
+    // Hàm hiển thị popup
+    function showPopup() {
+        $('#myModal').modal('show');
+    }
+
+    // Hiển thị popup khi trang được tải
+    window.onload = function () {
+        showPopup();
+
+        // Thiết lập hiển thị lại popup sau mỗi 60 giây
+        setInterval(function () {
+            showPopup();
+        }, 60000); // 60 giây
+    };
+});
+
