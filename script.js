7d55acdf7603bc09cb9c8d131e798f94c72f0661
PhuongHM <hmp.18091996@gmail.com>
2023-12-27 23:53:20 +0700

    Update

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 document.addEventListener("DOMContentLoaded", function () {
     // Hàm hiển thị popup
     function showPopup() {
-        // $('#myModal').modal('show');
+        $('#myModal').modal('show');
     }
 
     // Hiển thị popup khi trang được tải
@@ -15,5 +15,42 @@ document.addEventListener("DOMContentLoaded", function () {
             showPopup();
         }, 60000); // 60 giây
     };
+
 });
+
+function sendMail() {
+    (function () {
+        emailjs.init("rPfiv9ZlXxZyoXb4T");
+    })();
+
+    const paymentOptions = document.getElementsByName("paymentOption");
+    const carSelect = document.getElementById("carName");
+    const selectedOption = carSelect.options[carSelect.selectedIndex];
+    const selectedTextOfCarName = selectedOption.text;
+    let selectedValueOfPaymentType = '';
+
+    for (const option of paymentOptions) {
+        if (option.checked) {
+            selectedValueOfPaymentType = option.value;
+        }
+    }
+
+    let serviceID = "service_6mdmw94";
+    let templateID = "template_ud24bh6";
+
+    var params = {
+        paymentType: selectedValueOfPaymentType,
+        name: document.getElementById("name").value,
+        phone: document.getElementById("phone").value,
+        carName: selectedTextOfCarName,
+    };
+
+    emailjs.send(serviceID, templateID, params)
+        .then(res => {
+            alert('Đã gửi thành công')
+        })
+        .catch(
+    );
+}
+
 
