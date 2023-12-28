1ed6f104b3cb159354e34c1700414009eea9d2d0
hmp18091996 <hmp.18091996@gmail.com>
2023-12-28 18:06:03 +0700

    Update D

--- script.js
+++ script.js
@@ -6,14 +6,48 @@ document.addEventListener("DOMContentLoaded", function () {
         // $('#myModal').modal('show');
     }
 
-    // Hiển thị popup khi trang được tải
     window.onload = function () {
         showPopup();
 
-        // Thiết lập hiển thị lại popup sau mỗi 60 giây
         setInterval(function () {
             showPopup();
-        }, 60000); // 60 giây
+        }, 60000);
     };
+
 });
+
+function sendMail() {
+    (function () {
+        emailjs.init("rPfiv9ZlXxZyoXb4T");
+    })();
+
+    const paymentOptions = document.getElementsByName("paymentOption");
+    const carSelect = document.getElementById("carName");
+    const selectedOption = carSelect.options[carSelect.selectedIndex];
+    const selectedTextOfCarName = selectedOption.text;
+    let selectedValueOfPaymentType = '';
+
+    for (const option of paymentOptions) {
+        if (option.checked) {
+            selectedValueOfPaymentType = option.value;
+        }
+    }
+
+    let serviceID = "service_6mdmw94";
+    let templateID = "template_ud24bh6";
+
+    var params = {
+        paymentType: selectedValueOfPaymentType,
+        name: document.getElementById("name").value,
+        phone: document.getElementById("phone").value,
+        carName: selectedTextOfCarName,
+    };
+
+    emailjs.send(serviceID, templateID, params)
+        .then(res => {
+            alert('Đã gửi thành công')
+        })
+        .catch();
+}
+
 
