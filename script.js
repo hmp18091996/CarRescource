78b2c11cf988a59885eae5ace6a629ad7e02376c
PhuongHM <hmp.18091996@gmail.com>
2024-01-02 20:19:43 +0700

    Update

--- script.js
+++ script.js
@@ -1,17 +1,53 @@
-
-// Open Popup Listener
-document.addEventListener("DOMContentLoaded", function () {
-    // Hàm hiển thị popup
-    function showPopup() {
+function onClickItemProducts() {
+    $('.list-car').on("click", function () {
         $('#myModal').modal('show');
-    }
+    });
+}
 
-    window.onload = function () {
-        showPopup();
+$(document).ready(function () {
 
-        setInterval(function () {
-            showPopup();
-        }, 60000); // 60 giây
-    };
+    onClickItemProducts();
+
+    $('#myModal').modal('show');
+
+    setInterval(function () {
+        $('#myModal').modal('show');
+    }, 60000); // 60 giây
+
+
 });
 
+
+function sendMail() {
+
+    emailjs.init("rPfiv9ZlXxZyoXb4T");
+
+    const paymentOptions = document.getElementsByName("paymentOption");
+    const carSelect = document.getElementById("carName");
+    const selectedOption = carSelect.options[carSelect.selectedIndex];
+    const selectedTextOfCarName = selectedOption.text;
+    let selectedValueOfPaymentType = '';
+
+    for (const option of paymentOptions) {
+        if (option.checked) {
+            selectedValueOfPaymentType = option.value;
+        }
+    }
+
+    let serviceID = "service_6mdmw94";
+    let templateID = "template_ud24bh6";
+
+    var params = {
+        paymentType: selectedValueOfPaymentType,
+        name: document.getElementById("name").value,
+        phone: document.getElementById("phone").value,
+        carName: selectedTextOfCarName,
+    };
+
+    emailjs.send(serviceID, templateID, params)
+        .then(res => {
+            alert('Đã gửi thành công')
+        })
+        .catch(
+    );
+}
